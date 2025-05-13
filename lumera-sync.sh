cd61d81e892c5519155b97f02207cd74102103a0
Kyronode <100916828+kyronode@users.noreply.github.com>
2025-05-13 18:06:22 +0700

    Update lumera-sync.sh

--- lumera-sync.sh
+++ lumera-sync.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 rpc_port=$(grep -m 1 -oP '^laddr = "\K[^"]+' "$HOME/.lumera/config/config.toml" | cut -d ':' -f 3)
 while true; do
   local_height=$(curl -s localhost:$rpc_port/status | jq -r '.result.sync_info.latest_block_height')
@@ -13,6 +12,5 @@ while true; do
   echo -e "\033[1;33mNode Height:\033[1;34m $local_height\033[0m \
 \033[1;33m| Network Height:\033[1;36m $network_height\033[0m \
 \033[1;33m| Blocks Left:\033[1;31m $blocks_left\033[0m"
-
   sleep 5
 done
